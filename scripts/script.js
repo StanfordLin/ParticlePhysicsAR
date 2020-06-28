4b97555f45df18f582591b3e1401a783f0c50c5b
Stanford Lin <stanlin1999@gmail.com>
2020-06-27 17:35:51 -0700

    sender not working

--- scripts/script.js
+++ scripts/script.js
@@ -18,6 +18,7 @@ const Scene = require('Scene');
 
 // Use export keyword to make a symbol available in scripting debug console
 export const Diagnostics = require('Diagnostics');
+const Patches = require('Patches');
 
 // To use variables and functions across files, use export/import keyword
 // export const animationDuration = 10;
@@ -33,3 +34,38 @@ export const Diagnostics = require('Diagnostics');
 
 // To log messages to the console
 // Diagnostics.log('Console message logged from the script.');
+
+var leanedLeftEvent;
+var leanedLeft = false;
+
+var leanedRightEvent;
+var leanedRight = false;
+
+var activeEvent = false;
+
+Patches.outputs.getPulse('LeanedRight').then( event => {
+    Diagnostics.log("Leaned Right")
+})
+
+Patches.outputs.getPulse('LeanedLeft').then(event => {
+    Diagnostics.log("Leaned Leijft")
+})
+
+
+Diagnostics.log("Found")
+// Patches.outputs.getPulse('LeanedLeft').then(event => {
+//     Diagnostics.log("Leaned left");
+//     console.log("Yeah?")
+//     leanedLeftEvent = event.subscribe(function (pulseEvent) {
+//         console.log("test")
+//         if (activeEvent == false) {
+//             activeEvent = true;
+//             leanedLeft = true;
+//             Diagnostics.log("Turning left...");
+//             Patches.inputs.setBoolean('LeaningLeft', leanedLeft);
+//             // timeOut('LeaningLeft', leanedLeft, 'left');
+//             leanedLeft = false;
+//         } else (Diagnostics.log("Active Event")
+//         );
+//     })
+// })
